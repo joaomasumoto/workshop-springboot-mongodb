e91880c4cc2a3e262bfdfa214cd9c8d04a83af02
Joao Masumoto <jgabriel.mp@hotmail.com>
2026-07-07 12:31:10 -0300

    Connect MongoDB using repository and service layers

--- src/main/java/com/joaomasumoto/workshopmongo/resources/UserResource.java
+++ src/main/java/com/joaomasumoto/workshopmongo/resources/UserResource.java
@@ -2,6 +2,8 @@ package com.joaomasumoto.workshopmongo.resources;
 
 
 import com.joaomasumoto.workshopmongo.domain.User;
+import com.joaomasumoto.workshopmongo.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,12 +17,12 @@ import java.util.List;
 @RequestMapping(value="/users")
 public class UserResource {
 
+    @Autowired
+    private UserService service;
+
     @GetMapping
     public ResponseEntity<List<User>> findAll() {
-        User naruto = new User("1", "Naruto Uzumaki", "naruto@gmail.com");
-        User sasuke = new User("2", "Sasuke Uchiha", "sasuke@gmail.com");
-        List<User> list = new ArrayList<>();
-        list.addAll(Arrays.asList(naruto, sasuke));
+        List<User> list = service.findAll();
         return ResponseEntity.ok().body(list);
     }
 }
