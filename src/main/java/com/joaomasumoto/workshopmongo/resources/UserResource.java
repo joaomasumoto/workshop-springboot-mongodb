1555f18a3113ff1de3bcb8605b3eec33e73f5e37
Joao Masumoto <jgabriel.mp@hotmail.com>
2026-07-07 22:39:48 -0300

    implement UserDTO pattern for user responses

--- src/main/java/com/joaomasumoto/workshopmongo/resources/UserResource.java
+++ src/main/java/com/joaomasumoto/workshopmongo/resources/UserResource.java
@@ -2,6 +2,7 @@ package com.joaomasumoto.workshopmongo.resources;
 
 
 import com.joaomasumoto.workshopmongo.domain.User;
+import com.joaomasumoto.workshopmongo.dto.UserDTO;
 import com.joaomasumoto.workshopmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value="/users")
@@ -19,9 +21,17 @@ public class UserResource {
     private UserService service;
 
     @GetMapping
-    public ResponseEntity<List<User>> findAll() {
+    public ResponseEntity<List<UserDTO>> findAll() {
         List<User> list = service.findAll();
-        return ResponseEntity.ok().body(list);
+
+        // Starts a stream from the list of users
+        List<UserDTO> listDTO = list.stream()
+                // Maps each User to a UserDTO
+                .map(UserDTO::new )
+                // Converts the stream back into a List
+                .toList();
+
+        return ResponseEntity.ok().body(listDTO);
     }
 }
 
