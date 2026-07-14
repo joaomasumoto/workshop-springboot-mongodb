539e0673a61ff07baa930b6c8c8a29be420858e3
Joao Masumoto <jgabriel.mp@hotmail.com>
2026-07-13 23:17:30 -0300

    feat: add post comments

--- src/main/java/com/joaomasumoto/workshopmongo/config/Instantiation.java
+++ src/main/java/com/joaomasumoto/workshopmongo/config/Instantiation.java
@@ -1,5 +1,6 @@
 package com.joaomasumoto.workshopmongo.config;
 
+import com.joaomasumoto.workshopmongo.domain.Comment;
 import com.joaomasumoto.workshopmongo.domain.Post;
 import com.joaomasumoto.workshopmongo.domain.User;
 import com.joaomasumoto.workshopmongo.dto.AuthorDTO;
@@ -11,6 +12,7 @@ import org.springframework.context.annotation.Configuration;
 
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.List;
 import java.util.TimeZone;
 
 @Configuration
@@ -37,12 +39,19 @@ public class Instantiation implements CommandLineRunner {
         User tsunade = new User(null, "Tsunade Sama", "tsuna@gmail.com");
         User orochimaru = new User(null, "Orochimaru Sannin", "orochi@gmail.com");
 
-        userRepository.saveAll(Arrays.asList(jiraya, tsunade, orochimaru));
+        userRepository.saveAll(List.of(jiraya, tsunade, orochimaru));
 
         Post post1 = new Post(null, sdf.parse("22/03/2025"), "Mission number 2314", "Sunagakure mission completed. Returning to Konoha.", new AuthorDTO(jiraya));
         Post post2 = new Post(null, sdf.parse("03/07/2026"), "Mission number 5588", "Infiltration phase successfull. Proceeding to stage two.", new AuthorDTO(orochimaru));
 
-        postRepository.saveAll(Arrays.asList(post1, post2));
+        Comment comment1 = new Comment("Great job. Use alternative routes while passing through Sunagakure territory as a precaution.", sdf.parse("22/03/2025"), new AuthorDTO(orochimaru));
+        Comment comment2 = new Comment("Take your time. If you can, please bring me some desert herbs. I'm trying to develop a new medicine.", sdf.parse("22/03/2025"), new AuthorDTO(tsunade));
+        Comment comment3 = new Comment("BE CAREF--- YOUR POSITION HAS BEEN COMPR--- ABORT THE MISSION NOW!", sdf.parse("03/07/2026"), new AuthorDTO(jiraya));
+
+        post1.getComments().addAll(List.of(comment1, comment2));
+        post2.getComments().add(comment3);
+
+        postRepository.saveAll(List.of(post1, post2));
 
         jiraya.getPosts().add(post1);
         userRepository.save(jiraya);
--- src/main/java/com/joaomasumoto/workshopmongo/domain/Post.java
+++ src/main/java/com/joaomasumoto/workshopmongo/domain/Post.java
@@ -6,7 +6,9 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serial;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 
@@ -22,6 +24,7 @@ public class Post implements Serializable {
     private String title;
     private String body;
     private AuthorDTO author;
+    private List<Comment> comments = new ArrayList<>();
 
     public Post() {
 
@@ -73,6 +76,14 @@ public class Post implements Serializable {
 
     public void setAuthor(AuthorDTO author) {
         this.author = author;
+    }
+
+    public List<Comment> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<Comment> comments) {
+        this.comments = comments;
     }
 
     @Override
